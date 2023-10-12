123b290c4e01b8b697ed8d92737c9cef1a4f9e23
Kartik Pattaswamy <kpattaswamy@pixielabs.ai>
2023-10-12 20:24:37 +0000

    Link mongo to the other pieces of code and fix startup errors
    Signed-off-by: Kartik Pattaswamy <kpattaswamy@pixielabs.ai>

--- src/stirling/source_connectors/socket_tracer/testing/containers/mongodb/client.py
+++ src/stirling/source_connectors/socket_tracer/testing/containers/mongodb/client.py
@@ -1,27 +1,43 @@
 import pymongo
 import os
+import time
+
+print("sleep done")
 
 print("Starting MongoDB client")
-#client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+time.sleep(60)
+
+client = pymongo.MongoClient("mongodb://localhost:27017/?timeoutMS=2000")
 
 db = client["carDB"]
 collection = db["car"]
 
 # Insert document
+
+print("Inserting document")
 car = { "name": "Honda" }
 resp = collection.insert_one(car)
 print(resp)
 
+
 # Find document
+print("Finding the inserted document")
 resp = collection.find_one()
 print(resp)
 
+
 # Update document
+print("Updating the document")
 newCar = { "$set": {"name": "Toyota"} }
 resp = collection.update_one(car, newCar)
 print(resp)
 
+# Find updated document
+print("Finding the updated document")
+resp = collection.find_one()
+print(resp)
+
 # Delete document
+print("Delete the document")
 resp = collection.delete_one({"name": "Toyota"})
 print(resp)
